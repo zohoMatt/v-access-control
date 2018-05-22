ed0263d3765eacc21e395330a0b13a5799ea5d63
zohoMatt <zuoh14@mails.tsinghua.edu.cn>
2018-05-22 11:04:42 +0800

    project: Provide module as a library.

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,31 @@
 const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
     entry: './main.js',
     output: {
         path: path.resolve(__dirname, './dist'),
-        filename: 'index.js'
+        filename: 'index.bundle.js',
+        library: 'VueAccess',
+        libraryTarget: 'umd'
     },
     module: {
         rules: [
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
-                loader: 'babel-loader'
+                use: {
+                    loader: 'babel-loader',
+                    options: { babelrc: true }
+                }
             }
         ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist'])
+    ],
+    watch: true,
+    watchOptions: {
+        poll: 3000
     }
 }
